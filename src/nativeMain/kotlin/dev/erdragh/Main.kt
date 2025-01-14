97fa89ae15d8c6297d328a101c56ef44c9ea1894
Erdragh <mail@erdragh.dev>
2025-01-14 16:08:32 +0100

    Implement basics of Shader class

--- src/nativeMain/kotlin/dev/erdragh/Main.kt
+++ src/nativeMain/kotlin/dev/erdragh/Main.kt
@@ -5,9 +5,11 @@ import glew.GL_COLOR_BUFFER_BIT
 import glew.GL_DEPTH_BUFFER_BIT
 import glew.glClear
 import kotlinx.cinterop.ExperimentalForeignApi
+import okio.Path.Companion.toPath
 
 @OptIn(ExperimentalForeignApi::class)
 fun main(args: Array<String>) = Context.initialized {
+    val drawShader = Shader("draw", "shaders/vertex.glsl".toPath(), "shaders/fragment.glsl".toPath())
     while (running) {
         glClear((GL_COLOR_BUFFER_BIT or GL_DEPTH_BUFFER_BIT).toUInt())
         swapBuffers()
---
+++ src/nativeMain/kotlin/dev/erdragh/Shader.kt
@@ -0,0 +1,162 @@
+package dev.erdragh
+
+import glew.*
+import kotlinx.cinterop.*
+import okio.BufferedSource
+import okio.FileSystem
+import okio.Path
+
+@OptIn(ExperimentalForeignApi::class)
+private fun compileShader(type: GLenum, sources: Map<GLenum, Path>): GLuint {
+    val path = sources[type] ?: throw IllegalArgumentException("No shader source for type: $type")
+    println("Loading: $path...")
+    val source = FileSystem.SYSTEM.read(path, BufferedSource::readByteString)
+
+    if (source.size < 1) throw RuntimeException("Empty shader source: $path")
+
+    // TODO: Handle include
+
+    // actually compile shader
+    val shader = glCreateShader!!(type)
+    GL.shaderSource(shader, 1, source)
+    glCompileShader!!(shader)
+
+    // print error msg if failed
+    if (GL.getShaderiv(shader, GL_COMPILE_STATUS.toUInt()) != GL_TRUE) memScoped {
+        val logLength = GL.getShaderiv(shader, GL_INFO_LOG_LENGTH.toUInt())
+        val log = allocArray<ByteVar>(logLength + 1)
+        glGetShaderInfoLog!!(shader, logLength, null, log)
+
+        val string = log.toKString()
+        glDeleteShader!!(shader)
+        throw RuntimeException("Shader compilation failed:\n$string")
+    }
+
+    return shader
+}
+
+@OptIn(ExperimentalForeignApi::class)
+class Shader(val name: String) {
+    var id: GLuint = 0u
+    private val sourceFiles: MutableMap<GLenum, Path> = mutableMapOf()
+
+    constructor(name: String, compute: Path) : this(name) {
+        setComputeSource(compute)
+        compile()
+    }
+    constructor(name: String, vertex: Path, fragment: Path) : this(name) {
+        setVertexSource(vertex)
+        setFragmentSource(fragment)
+        compile()
+    }
+    constructor(name: String, vertex: Path, geometry: Path, fragment: Path) : this(name) {
+        setVertexSource(vertex)
+        setGeometrySource(geometry)
+        setFragmentSource(fragment)
+        compile()
+    }
+
+    private fun setVertexSource(path: Path) = setSource(GL_VERTEX_SHADER.toUInt(), path)
+    private fun setGeometrySource(path: Path) = setSource(GL_GEOMETRY_SHADER.toUInt(), path)
+    private fun setFragmentSource(path: Path) = setSource(GL_FRAGMENT_SHADER.toUInt(), path)
+    private fun setComputeSource(path: Path) = setSource(GL_COMPUTE_SHADER.toUInt(), path)
+    private fun setSource(type: GLenum, path: Path) {
+        sourceFiles[type] = path
+    }
+
+    private fun compile() {
+        // compile shaders
+        val program = glCreateProgram!!()
+        if (sourceFiles.containsKey(GL_COMPUTE_SHADER.toUInt())) {
+            // it's a compute shader
+            val shader = compileShader(GL_COMPUTE_SHADER.toUInt(), sourceFiles)
+            if (shader == 0u) {
+                glDeleteProgram!!(program)
+                return
+            }
+            glAttachShader!!(program, shader)
+        } else {
+            // is pipeline
+            if (sourceFiles.containsKey(GL_VERTEX_SHADER.toUInt())) {
+                val shader = compileShader(GL_VERTEX_SHADER.toUInt(), sourceFiles)
+                if (shader == 0u) {
+                    glDeleteProgram!!(program)
+                    return
+                }
+                glAttachShader!!(program, shader)
+            }
+
+            if (sourceFiles.containsKey(GL_GEOMETRY_SHADER.toUInt())) {
+                val shader = compileShader(GL_GEOMETRY_SHADER.toUInt(), sourceFiles)
+                if (shader == 0u) {
+                    glDeleteProgram!!(program)
+                    return
+                }
+                glAttachShader!!(program, shader)
+            }
+
+            if (sourceFiles.containsKey(GL_FRAGMENT_SHADER.toUInt())) {
+                val shader = compileShader(GL_FRAGMENT_SHADER.toUInt(), sourceFiles)
+                if (shader == 0u) {
+                    glDeleteProgram!!(program)
+                    return
+                }
+                glAttachShader!!(program, shader)
+            }
+        }
+
+        // link program
+        glLinkProgram!!(program)
+        if (GL.getProgramiv(program, GL_LINK_STATUS.toUInt()) != GL_TRUE) memScoped {
+            val logLength = GL.getProgramiv(id, GL_INFO_LOG_LENGTH.toUInt())
+            val log = allocArray<ByteVar>(logLength + 1)
+            glGetProgramInfoLog!!(id, logLength, null, log)
+
+            val string = log.toKString()
+            throw RuntimeException("Failed to link program from sources:\n${sourceFiles.values.joinToString("\n")}\n\n$string")
+        }
+
+        if (glIsProgram!!(id) == GL_TRUE.toUByte()) glDeleteProgram!!(id)
+        id = program
+    }
+
+    fun dispatchCompute(width: UInt, height: UInt = 1u, depth: UInt = 1u, memoryBarrierBits: GLbitfield = GL_ALL_BARRIER_BITS) {
+        TODO()
+    }
+
+    // Uniforms
+    fun uniform(name: String, value: Int) = memScoped {
+        val loc = glGetUniformLocation!!(id, name.cstr.ptr)
+        glUniform1i!!(loc, value)
+    }
+    fun uniform(name: String, value: IntArray) = memScoped {
+        value.usePinned { pinned ->
+            val loc = glGetUniformLocation!!(id, name.cstr.ptr)
+            glUniform1iv!!(loc, value.size, pinned.addressOf(0))
+        }
+    }
+
+    fun uniform(name: String, value: UInt) = memScoped {
+        val loc = glGetUniformLocation!!(id, name.cstr.ptr)
+        glUniform1ui!!(loc, value)
+    }
+    fun uniform(name: String, value: UIntArray) = memScoped {
+        value.usePinned { pinned ->
+            val loc = glGetUniformLocation!!(id, name.cstr.ptr)
+            glUniform1uiv!!(loc, value.size, pinned.addressOf(0))
+        }
+    }
+
+    fun uniform(name: String, value: Float) = memScoped {
+        val loc = glGetUniformLocation!!(id, name.cstr.ptr)
+        glUniform1f!!(loc, value)
+    }
+    fun uniform(name: String, value: FloatArray) = memScoped {
+        value.usePinned { pinned ->
+            val loc = glGetUniformLocation!!(id, name.cstr.ptr)
+            glUniform1fv!!(loc, value.size, pinned.addressOf(0))
+        }
+    }
+
+    // TODO: Vector uniforms
+}
